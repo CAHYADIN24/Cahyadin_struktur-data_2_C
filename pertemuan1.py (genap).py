f2eb30195ed26e333edb7ad2b92357a3aee80a9b
Cahyadin <cahyaww87@gmail.com>
2026-04-19 18:38:26 +0800

    Add files via upload

---
+++ pertemuan1.py (genap).py
@@ -0,0 +1,13 @@
+#("NAMA :Cahyadin")
+#("NIM  :25241082")
+#("KELAS:PTI C")
+
+nama = input("masukan nama anda :")
+if nama=="Cahyadin" :
+    print("selamat datang Cahyadin NIM anda adalah")
+    print("25241081")
+
+else :
+    print("nama salah")
+    print("anda tidak terdaftar")
+print("program selesai")
